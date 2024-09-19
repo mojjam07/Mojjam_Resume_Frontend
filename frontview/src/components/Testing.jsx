2099120996ce56d945cdd5cd4092d3646c147ed5
Jamiu Mojeed <mojjam07@gmail.com>
2024-09-19 01:34:29 +0100

    for testing sake

--- frontview/src/components/Testing.jsx
+++ frontview/src/components/Testing.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../styles/testing.css";
 // import { getNew } from "../services/api";
+import { getNew } from "../../";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
