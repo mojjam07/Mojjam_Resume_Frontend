1a5c4530d5ec77a5090c017ea7d8c51bf7ab2c36
Jamiu Mojeed <mojjam07@gmail.com>
2024-09-19 12:54:46 +0100

    broswer icon and title edit

--- frontview/vite.config.js
+++ frontview/vite.config.js
@@ -1,10 +1,12 @@
-import { build, defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
-  },
+    outDir: 'build' // Change this to 'dist' if you prefer 'dist' as the output folder
+  }
 });
+
+
